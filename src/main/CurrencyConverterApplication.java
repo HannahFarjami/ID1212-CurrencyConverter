587b4a277c54c1a1e251f60cb4b4d90d71bcee91
SimonNilbrink <simon.a.nilbrink@gmail.com>
2018-12-15 18:52:53 +0100

    Fixed so we get values from an API

--- src/main/CurrencyConverterApplication.java
+++ src/main/CurrencyConverterApplication.java
@@ -1,5 +1,8 @@
 
 package main;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import main.domain.ConversionPair;
 import main.domain.ConversionRate;
 import main.domain.Counter;
@@ -10,7 +13,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.stream.Stream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 @SpringBootApplication
 public class CurrencyConverterApplication {
@@ -32,8 +39,9 @@ public class CurrencyConverterApplication {
                     if(i!=j) {
                         rate = new ConversionRate(new ConversionPair(currency[i], currency[j]));
                         ConversionRate rate2 = new ConversionRate(new ConversionPair(currency[j], currency[i]));
-                        rate.setRate(1.1);
-                        rate2.setRate(1/1.1);
+                        double r = getConversionRate(currency[i],currency[j]);
+                        rate.setRate(r);
+                        rate2.setRate(1/r);
                         repo.save(rate2);
                         System.out.println(currency[i]+"/"+currency[j]);
                     }else{
@@ -44,6 +52,26 @@ public class CurrencyConverterApplication {
 
                 }
             }
+            System.out.println("Done");
+
         };
+    }
+
+
+    double getConversionRate(String to, String from){
+        double rate = 0;
+        try {
+            URL url = new URL("https://api.exchangeratesapi.io/latest?base="+from+"&symbols="+to);
+            URLConnection request = url.openConnection();
+            request.connect();
+
+            JsonParser jp = new JsonParser();
+            JsonObject root = jp.parse(new InputStreamReader((InputStream) request.getContent())).getAsJsonObject();
+            JsonObject rates = root.get("rates").getAsJsonObject();
+            rate = rates.get(to).getAsDouble();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return rate;
     }
 }
