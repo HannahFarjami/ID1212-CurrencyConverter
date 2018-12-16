e2a3b5bc34bcb1c63744a2c044ee691875fc9cc3
SimonNilbrink <simon.a.nilbrink@gmail.com>
2018-12-16 11:19:58 +0100

    Fixed the last on the admin page

--- src/main/presentation/AdminController.java
+++ src/main/presentation/AdminController.java
@@ -5,6 +5,7 @@ import main.application.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,14 +22,23 @@ public class AdminController {
 
     @GetMapping("/admin")
     public String adminPage(ChangeRateForm changeRateForm, Model model){
-        Integer counterValue = adminService.getCounter();
-        model.addAttribute(counterValue);
+        model.addAttribute(getCounterValue());
         return "adminPage";
     }
 
     @PostMapping("/update")
-    public String updateRate(ChangeRateForm changeRateForm){
+    public String updateRate(@Valid ChangeRateForm changeRateForm, BindingResult res, Model model){
+        model.addAttribute(getCounterValue());
+        if(res.hasErrors()) {
+            return "adminPage";
+        }
         adminService.updateRate(changeRateForm.getFrom(),changeRateForm.getTo(),changeRateForm.getNewRate());
-        return "redirect:admin";
+        model.addAttribute("Rate for "+changeRateForm.getFrom()+"/"+changeRateForm.getTo()+" has been changed");
+        return "adminPage";
+    }
+
+
+    private int getCounterValue(){
+        return adminService.getCounter();
     }
 }
